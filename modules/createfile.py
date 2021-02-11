30ced646eba5434ce80538bd02f223b3a28beded
cyla00 <ikhayam000@protonmail.com>
2021-02-11 14:19:16 +0100

    first stage

--- modules/createfile.py
+++ modules/createfile.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from modules.getdir import getDesktop
-from proxy_checker import ProxyChecker
 
 downloadLink = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all"
 list = requests.get(downloadLink, allow_redirects=True)
@@ -11,11 +10,6 @@ def createTxt():
     os.chdir(desktop)
     fileName = "socks5"
     fileType = ".txt"
-
-    for i in list:
-        checker = ProxyChecker()
-        checked = checker.check_proxy(i)
-        print(checked)
-    # file = open(fileName + fileType, "wb").write(list.content)
-    # print(f"{file} created on {desktop}")
+    file = open(fileName + fileType, "wb").write(list.content)
+    print(f"{file} created on {desktop}")
 
